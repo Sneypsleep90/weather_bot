746ce94eef5cd26a24b13317a982baefceb80320
magomedbersanov <magamed4924@mail.ru>
2025-09-16 10:58:04 +0300

    updates

--- clients/handler/handler.go
+++ clients/handler/handler.go
@@ -1,23 +1,33 @@
 package handler
 
 import (
+	"context"
 	"fmt"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 	"math"
-	"time"
 	"weatherr_bot/clients/openweather"
+	"weatherr_bot/models"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+type UserRepository interface {
+	GetUserCity(ctx context.Context, userId int64) (string, error)
+	CreateUser(ctx context.Context, userId int64) error
+	UpdateCity(ctx context.Context, userId int64, city string) error
+	GetUser(ctx context.Context, userId int64) (*models.User, error)
+}
 type Handler struct {
 	bot      *tgbotapi.BotAPI
 	owClient *openweather.OpenWeatherClient
+	userRepo UserRepository
 }
 
-func New(bot *tgbotapi.BotAPI, owClient *openweather.OpenWeatherClient) *Handler {
+func New(bot *tgbotapi.BotAPI, owClient *openweather.OpenWeatherClient, userRepo UserRepository) *Handler {
 	return &Handler{
 		bot:      bot,
 		owClient: owClient,
+		userRepo: userRepo,
 	}
 
 }
@@ -43,42 +53,111 @@ func (h *Handler) HandleUpdate(update tgbotapi.Update) {
 	if update.Message.IsCommand() && update.Message.Command() == "start" {
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, sendHello)
 		h.bot.Send(msg)
+		return
 	}
 
-	if update.Message.IsCommand() && update.Message.Command() == "time" {
-		currentTime := time.Now().Format("15:04:05")
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Время: %s", currentTime))
-		h.bot.Send(msg)
+	ctx := context.Background()
 
-	}
-
-	if !update.Message.IsCommand() {
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		coordinates, err := h.owClient.Coordinates(update.Message.Text)
+	if update.Message.IsCommand() {
+		err := h.ensureUser(ctx, update)
 		if err != nil {
-			log.Println(err)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не смогли получить город!")
-			msg.ReplyToMessageID = update.Message.MessageID
-			h.bot.Send(msg)
-			return
-		}
-		weather, err := h.owClient.Weather(coordinates.Lat, coordinates.Lon)
-		if err != nil {
-			log.Println(err)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%w", err))
+			log.Println("error ensureUser: ", err)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "произшала ошибка")
 			msg.ReplyToMessageID = update.Message.MessageID
 			h.bot.Send(msg)
 			return
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID,
-			fmt.Sprintf("Погода в %s %d градусов", update.Message.Text, int(math.Round(weather.Temp))),
-		)
+		switch update.Message.Command() {
+		case "city":
+			h.HandleSetCity(ctx, update)
+			return
+		case "weather":
+			h.HandleSendWeather(ctx, update)
+			return
+		default:
+			h.HandleUnknownCommand(update)
+			return
+
+		}
+
+	}
+}
+
+func (h *Handler) ensureUser(ctx context.Context, update tgbotapi.Update) error {
+	user, err := h.userRepo.GetUser(ctx, update.Message.From.ID)
+	if err != nil {
+		return fmt.Errorf("error userRepo.GetUser: %w", err)
+	}
+	if user == nil {
+		err := h.userRepo.CreateUser(ctx, update.Message.From.ID)
+		if err != nil {
+			return fmt.Errorf("error userRepo.CreateUser: %w", err)
+		}
+	}
+	return nil
+}
+
+func (h *Handler) HandleSetCity(ctx context.Context, update tgbotapi.Update) {
+	city := update.Message.CommandArguments()
+	if err := h.userRepo.CreateUser(ctx, update.Message.From.ID); err != nil {
+		log.Println("error userRepo.CreateUser: ", err)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "произшала ошибка")
 		msg.ReplyToMessageID = update.Message.MessageID
-
 		h.bot.Send(msg)
+		return
+	}
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Город %s сохранен", city))
+	msg.ReplyToMessageID = update.Message.MessageID
+	h.bot.Send(msg)
 
+}
+
+func (h *Handler) HandleSendWeather(ctx context.Context, update tgbotapi.Update) {
+	city, err := h.userRepo.GetUserCity(ctx, update.Message.From.ID)
+	if err != nil {
+		log.Println("error userRepo.GetUserCity : ", err)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "произшала ошибка")
+		msg.ReplyToMessageID = update.Message.MessageID
+		h.bot.Send(msg)
+		return
+	}
+	if city == "" {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "сначала установите город!")
+		msg.ReplyToMessageID = update.Message.MessageID
+		h.bot.Send(msg)
+		return
 	}
 
+	coordinates, err := h.owClient.Coordinates(city)
+	if err != nil {
+		log.Printf("error owClient.Coordinates : %w", err)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Не смогли получить город!")
+		msg.ReplyToMessageID = update.Message.MessageID
+		h.bot.Send(msg)
+		return
+	}
+	weather, err := h.owClient.Weather(coordinates.Lat, coordinates.Lon)
+	if err != nil {
+		log.Printf("error owClient.Weather: %w", err)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%w", err))
+		msg.ReplyToMessageID = update.Message.MessageID
+		h.bot.Send(msg)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID,
+		fmt.Sprintf("Погода в %s %d градусов", city, int(math.Round(weather.Temp))),
+	)
+	msg.ReplyToMessageID = update.Message.MessageID
+
+	h.bot.Send(msg)
+}
+
+func (h *Handler) HandleUnknownCommand(update tgbotapi.Update) {
+	log.Printf("unknow command [%s]%s", update.Message.From.UserName, update.Message.Text)
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "пока такой команды нет")
+	msg.ReplyToMessageID = update.Message.MessageID
+	h.bot.Send(msg)
 }
--- main.go
+++ main.go
@@ -1,31 +1,49 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
+	"context"
 	"log"
 	"os"
 	"weatherr_bot/clients/handler"
 	"weatherr_bot/clients/openweather"
+	"weatherr_bot/repo"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
 	err := godotenv.Load()
+
+	conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("error connecting to db", err)
+	}
+	defer conn.Close()
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		log.Fatal("error ping db")
+	}
+
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-	
+
+	usesRepo := repo.New(conn)
+
 	owClient := openweather.New(os.Getenv("OPENWEATHERAPI_KEY"))
-	botHandler := handler.New(bot, owClient)
+	botHandler := handler.New(bot, owClient, usesRepo)
 
 	botHandler.Start()
 
